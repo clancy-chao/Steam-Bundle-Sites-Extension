98cdd2e96033fdcf2031b1326d852b2d1c04b9e3
clancy-chao <clancy0613@gmail.com>
2017-10-26 21:09:32 +1300

    Rename SteamBundleSitesExtension.user.js to SBSE.user.js

--- SBSE.user.js
+++ SBSE.user.js
@@ -1124,9 +1124,9 @@ const siteHandlers = {
                 if (game) {
                     game.click();
                     setTimeout(() => {
-                        const $popup = $('.sr-warning-modal-buttons');
+                        const $popup = $('.sr-wans');
                         const skipOwned = !!$('.SBSE_ChkSkipOwned:checked').length;
-                        const selector = skipOwned ? '.sr-warning-modal-cancel-button' : '.sr-warning-modal-confirm-button';
+                        const selector = skipOwned ? '.sr-wal-button' : '.sr-warm-button';
 
                         if ($popup.length > 0) $popup.find(selector).click();
 
@@ -1135,7 +1135,7 @@ const siteHandlers = {
                 } else callback();
             };
 
-            bundleSitesBoxHandler.reveal(handler, $('.sr-unredeemed-steam-button'));
+            bundleSitesBoxHandler.reveal(handler, $('.sr-unredn'));
         });
         $('.SBSE_BtnRetrieve').click(() => {
             bundleSitesBoxHandler.retrieve(extractKeys());
