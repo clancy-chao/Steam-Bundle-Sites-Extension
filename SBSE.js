fdc9ac73016e419d10f8a936c3f4cde679b4c5c0
clancy-chao <clancy0613@gmail.com>
2018-10-02 19:41:29 +1300

    Update SBSE.js

--- SBSE.js
+++ SBSE.js
@@ -2,7 +2,7 @@
 // @name         Steam Bundle Sites Extension
 // @homepage     https://github.com/clancy-chao/Steam-Bundle-Sites-Extension
 // @namespace    http://tampermonkey.net/
-// @version      2.6.0
+// @version      2.6.1
 // @updateURL    https://github.com/clancy-chao/Steam-Bundle-Sites-Extension/raw/master/SBSE.meta.js
 // @downloadURL  https://github.com/clancy-chao/Steam-Bundle-Sites-Extension/raw/master/SBSE.user.js
 // @description  A steam bundle sites' tool kits.
@@ -3497,7 +3497,7 @@ const siteHandlers = {
                     items: [],
                 };
 
-                $('.deliver-gkey:contains(-)').each((i, ele) => {
+                $('.deliver-gkey > *:contains(-)').each((i, ele) => {
                     const $ele = $(ele);
                     const d = {
                         title: $ele.parent().prev().text().trim(),
