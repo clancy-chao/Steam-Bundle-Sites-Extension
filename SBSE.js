bf57aba94fa0bed0b2b6fe26d7b5fe720cbdaee7
clancy-chao <clancy0613@gmail.com>
2018-10-07 08:48:15 +1300

    Update SBSE.js

--- SBSE.js
+++ SBSE.js
@@ -2,7 +2,7 @@
 // @name         Steam Bundle Sites Extension
 // @homepage     https://github.com/clancy-chao/Steam-Bundle-Sites-Extension
 // @namespace    http://tampermonkey.net/
-// @version      2.7.0
+// @version      2.7.1
 // @updateURL    https://github.com/clancy-chao/Steam-Bundle-Sites-Extension/raw/master/SBSE.meta.js
 // @downloadURL  https://github.com/clancy-chao/Steam-Bundle-Sites-Extension/raw/master/SBSE.user.js
 // @description  A steam bundle sites' tool kits.
@@ -22,11 +22,11 @@
 // @include      https://steamdb.steamcn.com/tooltip*
 // @include      https://yuplay.ru/orders/*/
 // @include      https://yuplay.ru/product/*/
-// @include      http://gama-gama.ru/personal/settings/*
-// @include      https://plati.ru/seller/*
-// @include      https://plati.market/seller/*
-// @include      https://po.plati.ru/seller/*
-// @include      https://po.plati.market/seller/*
+// @include      http*://gama-gama.ru/personal/settings/*
+// @include      http*://plati.ru/seller/*
+// @include      http*://plati.market/seller/*
+// @include      http*://po.plati.ru/seller/*
+// @include      http*://po.plati.market/seller/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.18.0/sweetalert2.min.js
 // @resource     sweetalert2CSS https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.18.0/sweetalert2.min.css
