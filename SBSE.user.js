d81a32c653de31f04b38c2b0fbd9ce6f3cce80c0
clancy-chao <clancy0613@gmail.com>
2020-06-30 20:46:23 +1200

    Update SBSE.user.js

--- SBSE.user.js
+++ SBSE.user.js
@@ -6,7 +6,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 // @name         Steam Bundle Sites Extension
 // @homepage     https://github.com/clancy-chao/Steam-Bundle-Sites-Extension
 // @namespace    http://tampermonkey.net/
-// @version      2.16.0
+// @version      2.16.1
 // @updateURL    https://github.com/clancy-chao/Steam-Bundle-Sites-Extension/raw/master/SBSE.meta.js
 // @downloadURL  https://github.com/clancy-chao/Steam-Bundle-Sites-Extension/raw/master/SBSE.user.js
 // @description  A steam bundle sites' tool kits.
